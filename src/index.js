419a79591f010b7086f43e82baae9b09b02f375d
Vladioska <vlad_y99@mail.ru>
2018-09-12 22:08:40 +0300

    LoveTriangles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,23 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+module.exports = function getLoveTrianglesCount(preferences = []) 
+{
+  var a = preferences.slice();
+  for(var i = 0; i < a.length; i++) a[i]--;
+  var ans = 0;
+
+  for(var i = 0; i < a.length; i++)
+  {
+      var FirstLove = a[i];
+      var SecondLove = a[FirstLove];
+      var ThirdLove = a[SecondLove];
+      
+      if(ThirdLove == i && FirstLove != SecondLove && SecondLove != ThirdLove) 
+         {
+        ans++;
+         }
+  }
+  return ans / 3;
 };
+
